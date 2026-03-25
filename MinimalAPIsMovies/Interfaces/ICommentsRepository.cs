55704bd50b59f8d567e6c6515ebe362d4963ae6d
beqa beraia <beka.beraia.2@btu.edu.ge>
2026-03-25 21:16:46 +0400

    added Comment entity, ICommentsRepository and CommentsRepository

---
+++ MinimalAPIsMovies/Interfaces/ICommentsRepository.cs
@@ -0,0 +1,15 @@
+﻿using MinimalAPIsMovies.DTOs;
+using MinimalAPIsMovies.Entities;
+
+namespace MinimalAPIsMovies.Interfaces
+{
+    public interface ICommentsRepository
+    {
+        Task<int> CreateAsync(Comment comment);
+        Task<List<Comment>> GetAllAsync(PaginationDTO paginationDTO);
+        Task<Comment?> GetByIdAsync(int id);
+        Task<bool> ExistsAsync(int id);
+        Task UpdateAsync(Comment comment);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ MinimalAPIsMovies/Repositories/CommentsRepository.cs
@@ -0,0 +1,56 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using MinimalAPIsMovies.DTOs;
+using MinimalAPIsMovies.Entities;
+using MinimalAPIsMovies.Interfaces;
+using System.Data;
+
+namespace MinimalAPIsMovies.Repositories
+{
+    public class CommentsRepository : ICommentsRepository
+    {
+        private readonly string _connectionString;
+
+        public CommentsRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection")!;
+        }
+
+        public async Task<int> CreateAsync(Comment comment)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var id = await connection.QuerySingleAsync<int>("Comments_Create",
+                    new { comment.Body, comment.MovieId }, commandType: CommandType.StoredProcedure);
+                comment.Id = id;
+
+                return id;
+            }
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ExistsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Comment>> GetAllAsync(PaginationDTO paginationDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Comment?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
