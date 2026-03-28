0c29c27eb8acd4892488e75dae063810ca20a059
beqa beraia <beka.beraia.2@btu.edu.ge>
2026-03-28 13:15:46 +0400

    added GetAll and GetById endpoints in CommentsEndpoints

--- MinimalAPIsMovies/Endpoints/CommentsEndpoints.cs
+++ MinimalAPIsMovies/Endpoints/CommentsEndpoints.cs
@@ -12,10 +12,13 @@ namespace MinimalAPIsMovies.Endpoints
         public static RouteGroupBuilder MapComments(this RouteGroupBuilder group)
         {
             group.MapPost("/", CreateAsync);
+            group.MapGet("/", GetAllAsync)
+                .CacheOutput(c => c.Expire(TimeSpan.FromMinutes(1)).Tag("comments-get"));
+            group.MapGet("/{id:int}", GetByIdAsync).WithName("GetCommentById");
             return group;
         }
 
-        static async Task<Results<Created<CommentDTO>, NotFound>> CreateAsync(int movieId,
+        static async Task<Results<CreatedAtRoute<CommentDTO>, NotFound>> CreateAsync(int movieId,
             CreateCommentDTO createCommentDTO, ICommentsRepository commentsRepository, 
             IMoviesRepository moviesRepository, IOutputCacheStore outputCacheStore, 
             IMapper mapper)
@@ -31,7 +34,43 @@ namespace MinimalAPIsMovies.Endpoints
             await outputCacheStore.EvictByTagAsync("comments-get", default);
             var commentDTO = mapper.Map<CommentDTO>(comment);
 
-            return TypedResults.Created($"/comment/{id}", commentDTO);
+            return TypedResults.CreatedAtRoute(commentDTO, "GetCommentById", new { id, movieId });
         }
+
+        static async Task<Results<Ok<List<CommentDTO>>, NotFound>> GetAllAsync(int movieId,
+            ICommentsRepository commentsRepository, IMoviesRepository moviesRepository,
+            IMapper mapper)
+        {
+            if (!await moviesRepository.ExistsAsync(movieId))
+            {
+                return TypedResults.NotFound();
+            }
+
+            var comments = await commentsRepository.GetAllAsync(movieId);
+            var commentsDTO = mapper.Map<List<CommentDTO>>(comments);
+
+            return TypedResults.Ok(commentsDTO);
+        }
+
+        static async Task<Results<Ok<CommentDTO>, NotFound>> GetByIdAsync(int movieId, int id,
+            ICommentsRepository commentsRepository, IMoviesRepository moviesRepository,
+            IMapper mapper)
+        {
+            if (!await moviesRepository.ExistsAsync(movieId))
+            {
+                return TypedResults.NotFound();
+            }
+
+            var comment = await commentsRepository.GetByIdAsync(id);
+
+            if (comment is null)
+            {
+                return TypedResults.NotFound();
+            }
+
+            var commentDTO = mapper.Map<CommentDTO>(comment);
+
+            return TypedResults.Ok(commentDTO);
+        } 
     }
 }
