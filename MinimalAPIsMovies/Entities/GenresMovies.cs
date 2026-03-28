35f0596ebfaf7d39c3d5e39ad3e274dd1ac710c3
beqa beraia <beka.beraia.2@btu.edu.ge>
2026-03-28 23:53:20 +0400

    added GenresMovies entity for manyToMany relationship between Genres and Movies

---
+++ MinimalAPIsMovies/Entities/GenresMovies.cs
@@ -0,0 +1,10 @@
+﻿namespace MinimalAPIsMovies.Entities
+{
+    public class GenresMovies
+    {
+        public int MovieId { get; set; }
+        public int GenreId { get; set; }
+        public Movie Movie { get; set; } = null!;
+        public Genre Genre { get; set; } = null!;
+    }
+}
