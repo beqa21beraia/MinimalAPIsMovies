4dacad5c0d972644504572895fde9d2c4e55ce02
beqa beraia <beka.beraia.2@btu.edu.ge>
2026-04-17 20:16:32 +0400

    feat(auth): add make admin and remove admin endpoints with validation

--- MinimalAPIsMovies/Endpoints/UsersEndpoints.cs
+++ MinimalAPIsMovies/Endpoints/UsersEndpoints.cs
@@ -7,6 +7,7 @@ using Microsoft.IdentityModel.Tokens;
 using MinimalAPIsMovies.DTOs;
 using MinimalAPIsMovies.Filters;
 using MinimalAPIsMovies.Utilities;
+using MinimalAPIsMovies.Validations;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -21,6 +22,14 @@ namespace MinimalAPIsMovies.Endpoints
                 .AddEndpointFilter<ValidationFilter<UserCredentialsDTO>>();
             group.MapPost("/login", Login)
                 .AddEndpointFilter<ValidationFilter<UserCredentialsDTO>>();
+
+            group.MapPost("/makeadmin", MakeAdmin)
+                .AddEndpointFilter<ValidationFilter<EditClaimDTO>>()
+                .RequireAuthorization("isadmin");
+
+            group.MapPost("/removeadmin", RemoveAdmin)
+                .AddEndpointFilter<ValidationFilter<EditClaimDTO>>()
+                .RequireAuthorization("isadmin");
             return group;
         }
 
@@ -72,6 +81,34 @@ namespace MinimalAPIsMovies.Endpoints
             }
         }
 
+        static async Task<Results<NoContent, NotFound>> MakeAdmin(EditClaimDTO editClaimDTO,
+            [FromServices] UserManager<IdentityUser> userManager)
+        {
+            var user = await userManager.FindByEmailAsync(editClaimDTO.Email);
+
+            if (user is null)
+            {
+                return TypedResults.NotFound();
+            }
+
+            await userManager.AddClaimAsync(user, new Claim("isadmin", "true"));
+            return TypedResults.NoContent();
+        }
+
+        static async Task<Results<NoContent, NotFound>> RemoveAdmin(EditClaimDTO editClaimDTO,
+            [FromServices] UserManager<IdentityUser> userManager)
+        {
+            var user = await userManager.FindByEmailAsync(editClaimDTO.Email);
+
+            if (user is null)
+            {
+                return TypedResults.NotFound();
+            }
+
+            await userManager.RemoveClaimAsync(user, new Claim("isadmin", "true"));
+            return TypedResults.NoContent();
+        }
+
         private async static Task<AuthenticationResponseDTO> BuildToken(
             UserCredentialsDTO userCredentialsDTO, UserManager<IdentityUser> userManager,
             IConfiguration configuration)
@@ -80,6 +117,11 @@ namespace MinimalAPIsMovies.Endpoints
             {
                 new Claim("email", userCredentialsDTO.Email)
             };
+
+            var user = await userManager.FindByNameAsync(userCredentialsDTO.Email);
+            var claimsFromDB = await userManager.GetClaimsAsync(user!);
+
+            claims.AddRange(claimsFromDB);
 
             var key = KeysHandler.GetKey(configuration).First();
             var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
